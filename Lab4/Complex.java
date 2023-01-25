9ea4e0d5ca2eb08a73841cac434bfa500b315935
3bdElsamea <asayed4761@gmail.com>
2023-01-25 15:45:14 +0200

    Java Lab4

--- Lab4/Complex.java
+++ Lab4/Complex.java
@@ -1,10 +1,12 @@
-class ComplexNumber<T extends Number> {
-    private T real;
-    private T imag;
+import java.math.BigDecimal;
 
-    public ComplexNumber(T real, T imag) {
-        this.real = real;
-        this.imag = imag;
+class ComplexNumber<T extends BigDecimal> {
+    private T real;
+    private T imaginary;
+
+    public ComplexNumber(T _real, T _imaginary) {
+        this.real = _real;
+        this.imaginary = _imaginary;
     }
 
     public T getReal() {
@@ -12,7 +14,18 @@ class ComplexNumber<T extends Number> {
     }
 
     public T getImag() {
-        return imag;
+        return imaginary;
+    }
+
+    // Adding Method
+    public ComplexNumber<T> addComplex(ComplexNumber<T> number) {
+        return new ComplexNumber<T>((T) this.real.add(number.real), (T) this.imaginary.add(number.imaginary));
+    }
+
+    // Subtracting Method
+    public ComplexNumber<T> subtractComplex(ComplexNumber<T> number) {
+        return new ComplexNumber<T>((T) this.real.subtract(number.real),
+                (T) this.imaginary.subtract(number.imaginary));
     }
 }
 
@@ -22,8 +35,19 @@ public class Complex {
         if (args.length == 4 &&
                 args[0].matches("-?\\d+(\\.\\d+)?") && args[1].matches("-?\\d+(\\.\\d+)?") &&
                 args[2].matches("-?\\d+(\\.\\d+)?") && args[3].matches("-?\\d+(\\.\\d+)?")) {
+            BigDecimal real1 = new BigDecimal(args[0]);
+            BigDecimal imaginary1 = new BigDecimal(args[1]);
+            BigDecimal real2 = new BigDecimal(args[2]);
+            BigDecimal imaginary2 = new BigDecimal(args[3]);
 
+            ComplexNumber<BigDecimal> complex1 = new ComplexNumber<>(real1, imaginary1);
+            ComplexNumber<BigDecimal> complex2 = new ComplexNumber<>(real2, imaginary2);
 
+            ComplexNumber<BigDecimal> result = complex1.addComplex(complex2);
+            System.out.println("Addition : " + result.getReal() + " + " + result.getImag() + "i");
+
+            result = complex1.subtractComplex(complex2);
+            System.out.println("Subtraction : " + result.getReal() + " + " + result.getImag() + "i");
         } else {
             System.out.println("Invalid Input");
         }
