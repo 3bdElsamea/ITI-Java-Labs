a064615b74373cc5f8ce380450b345e0d2973497
3bdElsamea <asayed4761@gmail.com>
2023-01-29 17:06:47 +0200

    GUI Lab 1

---
+++ Lab8/CharWordCounter/src/charwordcounter/CharWordCounter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package charwordcounter;
+
+/**
+ *
+ * @author lenovo
+ */
+public class CharWordCounter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+
+}
---
+++ Lab8/IpFinder/src/ipfinder/IpFinder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipfinder;
+
+/**
+ *
+ * @author lenovo
+ */
+public class IpFinder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IP_Finder ipFinder=new IP_Finder();
+        ipFinder.setVisible(true);
+    }
+    
+}
